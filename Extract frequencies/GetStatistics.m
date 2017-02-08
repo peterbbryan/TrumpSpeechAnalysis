139d366b5688a68fe00dcee405cba9ae6da55427
Peter Bryan <peterbbryan@gmail.com>
2017-02-08 23:05:01 +0000

    Initial analyses to just get the token frequencies

---
+++ Extract frequencies/GetStatistics.m
@@ -0,0 +1,48 @@
+clear all; close all;
+
+[~,RAW_TRUMP,~] = xlsread('trump_words');
+[~,~,RAW_SUBTLEX] = xlsread('SUBTLEXusExcel2007.xlsx');
+
+unique_tokens = unique(RAW_TRUMP');
+
+variable_names = RAW_SUBTLEX(1,:);
+freq_table = cell2table(RAW_SUBTLEX(2:end,:),'VariableNames',variable_names);
+
+trump_frequencies = nan(1,numel(unique_tokens));
+general_frequencies = nan(1,numel(unique_tokens));
+
+freq_table.FREQcount = zscore(freq_table.FREQcount);
+
+for word_ind = 1:numel(unique_tokens)
+    word = unique_tokens(word_ind);
+    trump_frequencies(1,word_ind) = sum(strcmp(word,RAW_TRUMP));
+    
+    row = find(strcmp(word, freq_table.Word));
+    if (isempty(row))
+        general_frequencies(:,word_ind) = 0;
+    else
+        general_frequencies(:,word_ind) = freq_table.FREQcount(row);
+    end
+end
+
+trump_frequencies = zscore(trump_frequencies);
+
+unique_tokens(general_frequencies == 0) = [];
+trump_frequencies(general_frequencies == 0) = [];
+general_frequencies(general_frequencies == 0) = [];
+
+difference = trump_frequencies-general_frequencies;
+
+[difference, order] = sort(difference);
+unique_tokens = unique_tokens(order);
+
+unique_tokens = unique_tokens(difference > 0);
+difference = difference(difference > 0);
+
+bar(log(difference+1));
+set(gca,'xtick',0:numel(unique_tokens));
+set(gca,'xticklabel',unique_tokens);
+ax.XTickLabelRotation=45;
+
+%unique_tokens = [unique_tokens(1:20) unique_tokens(end-19:end)];
+%difference = [difference(1:20) difference(end-19:end)];
---
+++ Word extraction/tokenizer.py
@@ -0,0 +1,22 @@
+import csv
+import glob
+import itertools
+import os
+import nltk
+import sys
+reload(sys)
+
+sys.setdefaultencoding("utf-8")
+
+with open("lexicon.txt", "r") as source:
+	LEXICON = frozenset(word.strip().lower() for word in source)
+
+with open('speeches.txt', 'r') as content_file:
+	content = content_file.read()
+	words = nltk.word_tokenize(content)
+	words = [word.lower() for word in words]
+	words = [word for word in words if (word in LEXICON and not (word == 'speech'))]
+		
+	with open("trump_words.csv",'w') as word_file:
+		for word in words:
+			word_file.write(word + "\n")	
