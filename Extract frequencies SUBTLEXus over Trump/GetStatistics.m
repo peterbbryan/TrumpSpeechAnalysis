b9880709cad7b06117504dcdeeeacd5dcce4a8d4
Peter Bryan <peterbbryan@gmail.com>
2017-02-09 22:40:34 +0000

    words infrequently used by Trump added

---
+++ Extract frequencies SUBTLEXus over Trump/GetStatistics.m
@@ -0,0 +1,59 @@
+clear all; close all;
+
+[~,RAW_TRUMP,~] = xlsread('trump_words');
+[~,~,RAW_SUBTLEX] = xlsread('SUBTLEXusExcel2007.xlsx');
+
+unique_tokens = unique(RAW_TRUMP');
+
+variable_names = RAW_SUBTLEX(1,:);
+freq_table = cell2table(RAW_SUBTLEX(2:end,:),'VariableNames',variable_names);
+
+trump_frequencies = nan(1,numel(unique_tokens));
+general_frequencies = nan(1,numel(unique_tokens));
+
+freq_table.FREQcount = zscore(freq_table.FREQcount);
+
+for word_ind = 1:numel(unique_tokens)
+    word = unique_tokens(word_ind);
+    trump_frequencies(1,word_ind) = sum(strcmp(word,RAW_TRUMP));
+    
+    row = find(strcmp(word, freq_table.Word));
+    if (isempty(row))
+        general_frequencies(:,word_ind) = 0;
+    else
+        general_frequencies(:,word_ind) = freq_table.FREQcount(row);
+    end
+end
+
+trump_frequencies = zscore(trump_frequencies);
+
+unique_tokens(general_frequencies == 0) = [];
+trump_frequencies(general_frequencies == 0) = [];
+general_frequencies(general_frequencies == 0) = [];
+
+difference = general_frequencies-trump_frequencies;
+
+[difference, order] = sort(difference);
+unique_tokens = unique_tokens(order);
+
+unique_tokens = unique_tokens(difference > 0);
+difference = difference(difference > 0);
+                       
+unique_tokens = unique_tokens(end-99:end);
+difference = difference(end-99:end);
+                       
+bar(log(difference+1),'FaceColor',[50 55 55]./100,'EdgeColor',[50 55 55]./100);
+set(gca,'xtick',1:numel(unique_tokens));
+xlim([1 numel(unique_tokens)]);
+set(gca,'xticklabel',unique_tokens);
+xtickangle(45);
+
+
+title('Words used more frequently by SUBTLEXus than Trump (log space, top 100)')
+ylabel('Z-score frequency (log)');
+xlabel('Word');
+
+
+
+%unique_tokens = [unique_tokens(1:20) unique_tokens(end-19:end)];
+%difference = [difference(1:20) difference(end-19:end)];
